14c61467e0c8011f8fa3e8ad54e1fd93e6fb4006
amm-ai <113884190+amm-ai@users.noreply.github.com>
2022-10-19 22:59:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+from deta import Deta
 
-import streamlit as st
 
 def main_page():
     st.markdown("# Main page 🎈")
@@ -13,6 +13,32 @@ def main_page():
 def page2():
     st.markdown("# Page 2 ❄️")
     st.sidebar.markdown("# Page 2 ❄️")
+    # Data to be written to Deta Base
+    with st.form("form"):
+        name = st.text_input("Your name")
+        age = st.number_input("Your age")
+        submitted = st.form_submit_button("Store in database")
+
+
+    # Connect to Deta Base with your Project Key
+    deta = Deta(st.secrets["deta_key"])
+
+    # Create a new database "example-db"
+    # If you need a new database, just use another name.
+    db = deta.Base("example-db")
+
+    # If the user clicked the submit button,
+    # write the data from the form to the database.
+    # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+    if submitted:
+        db.put({"name": name, "age": age})
+
+    "---"
+    "Here's everything stored in the database:"
+    # This reads all items from the database and displays them to your app.
+    # db_content is a list of dictionaries. You can do everything you want with it.
+    db_content = db.fetch().items
+    st.write(db_content)
 
 def page3():
     st.markdown("# Page 3 🎉")
