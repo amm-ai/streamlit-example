7f37fef3ca57b253419b9f0ac9ea9e661ab7f5a4
amm-ai <113884190+amm-ai@users.noreply.github.com>
2022-10-19 23:07:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,15 +54,9 @@ page_names_to_funcs = {
 
 def check_password():
     """Returns `True` if the user had a correct password."""
-    """### gif from local file"""
-    file_ = open("comp.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
+   
 
-    st.markdown(
-        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
-        unsafe_allow_html=True,)
+   
    
     def password_entered():
         """Checks whether a password entered by the user is correct."""
