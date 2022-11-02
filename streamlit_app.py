e55411da62d6e32c4b1f0075ee2bc70c6e32a5b9
amm-ai <113884190+amm-ai@users.noreply.github.com>
2022-11-02 22:23:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ conn = init_connection()
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
-def run_query(query):
+def run_query(query,record):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
@@ -31,8 +31,8 @@ with st.form("form", clear_on_submit=True):
     record = (n,c)
   
 if submitted:
-    QUERY = """INSERT INTO CHICKEN_TABLE (n,c) VALUES (%s,%s);"""
-    run_query(QUERY,record)
+    query = """INSERT INTO CHICKEN_TABLE (n,c) VALUES (%s,%s);"""
+    run_query(query,record)
     data = run_query("SELECT * from CHICKEN_TABLE;")
     # Print results.
     st.write(data)
